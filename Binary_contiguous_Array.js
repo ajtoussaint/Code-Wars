e111a0a8ae2631c366c7ec79178346cf369f5e77
Andrew Toussaint <ajntoussaint@gmail.com>
2023-05-25 06:59:06 -0400

    05/25/23 I cheated and looked it up

--- Binary_contiguous_Array.js
+++ Binary_contiguous_Array.js
@@ -1,56 +1,35 @@
+// An Array consisting of "0" and "1"'s also called a binary array is given as an input.
+//
+// Task
+//
+// Find the length of the longest contiguous subarray which consists of Equal number of "0"s and "1"s.
+//
+// Example
+//
+// s = [1,1,0,1,1,0,1,1]
+//          |_____|
+//             |
+//          [0,1,1,0]
+//
+//          length = 4
+// Note
+//
+// 1<=length(array)<120000
+
+
 function binarray(a) {
-  let obj = {1:0, 0:0}
-  let indexer = {1:[],0:[]}
-  a.forEach( (num,i) => {
-    obj[num]++;
-    indexer[num].push(i)
-  })
-  //if there is only 1s or 0s in the array then the answer is 0
-  if(obj[1] === 0 || obj[0] === 0) return 0;
-  let mainLength = a.length
-  if(mainLength === 2) return 2;
-
-  //determine which is less common 1 or 0 and save it
-  let minority = obj[1] < obj[0] ? 1 : 0; //zero will be minority if even
-
-
-  //find all of the less common indexes and save in an array
-  let indecies = indexer[minority];
-
-
-  //find the largest possible even array
-  //largest possible array size is the smaller of 1s or 0s x2
-  let size = 2*obj[minority];
-  //array must be even length
-  if(size % 2 !== 0)size--;
-
-
-  //for this array size
-   //check if the array capturing the 0th indicies and the size/2th indicies
-   // if so return the size
-  //loop through index array and then reduce the size by 2
-  console.time("main");
-  let counter = 0;
-  while(size > 0){
-    for(let j=0; j+size/2<=indecies.length; j++){
-      counter++;
-      if(indecies[j+size/2-1] - indecies[j] + 1 <= size){
-        console.log("Looops: ", counter, "MainL: ", mainLength, "Final size:" ,size);
-        console.timeEnd("main");
-        return size;
-        //aparently this is all useless or they don't test the edge case?
-        /*let precede = j === 0 ? indecies[j] : indecies[j] - indecies[j-1] -1;
-        let follow = j+size/2 === indecies.length ? mainLength - indecies[j] - 1 : indecies[j+size/2] - indecies[j+size/2-1]-1;
-        if(indecies[j+size/2-1] - indecies[j]  + precede + follow >= size){
-          console.log("Loops: ", counter);
-          console.timeEnd("main");
-          return size;
-
-        }*/
-      }
+  let max = 0;
+  let hash = {}
+  let sum = 0;
+  for(let i = 0; i < a.length; i++){
+    sum += 2*a[i] - 1;
+    if(sum === 0){
+      max = i + 1;
+    }else if(hash[sum]>=0){
+      max = max < i - hash[sum] ? i - hash[sum] : max;
+    }else{
+      hash[sum] = i;
     }
-    size -= 2;
   }
-  console.timeEnd("main");
-  return 0;
+  return max;
 }

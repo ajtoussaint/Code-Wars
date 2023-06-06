b56da129f76533feb91cadfd57a01a21c95bedce
Andrew Toussaint <ajntoussaint@gmail.com>
2023-06-06 06:21:31 -0400

    06/06/23

--- Bit_Counting.js
+++ Bit_Counting.js
@@ -3,5 +3,5 @@
 // Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
 
 var countBits = function(n) {
-    return n.toString(2).split("0").join("").length;;
+    return n.toString(2).split("0").join("").length;
   };

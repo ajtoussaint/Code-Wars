a08a9fa55a624dd645214745c1334b93a0243118
Andrew Toussaint <ajntoussaint@gmail.com>
2023-06-07 19:20:05 -0500

    Update Greedy_Theif.js
    06/07/23 Revisiting Greedy Thief with help

--- Greedy_Theif.js
+++ Greedy_Theif.js
@@ -1,89 +1,29 @@
 function greedyThief(items, n){
-  class Bag{
-    constructor(contents, possible, weight, limit, price){
-      this.contents = contents;
-      this.possible = possible;
-      this.weight = weight;
-      this.limit = limit;
-      this.price = price;
-    }
-    transfer(index){
-      let newWeight = this.weight + this.possible[index].weight;
-      let newPrice = this.price + this.possible[index].price;
-      if(newWeight <= this.limit){
-        let newContents = [...this.contents, this.possible[index]];
-        let newPossible = [...this.possible];
-        newPossible.splice(index,1);
-        let newBag = new Bag(newContents, newPossible, newWeight, this.limit, newPrice);
-        return newBag;
+  //credit to Geeks for Geeks: https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
+  let length = items.length;
+  let table = new Array(length+1);//maybe start empty and push, more my style
+  for(let i = 0; i <= length; i++){
+    table[i] = new Array(n+1);//maybe start empty and push, more my style
+    for(let w = 0; w <= n; w++){
+      //base case, now weight or no items = 0 value
+      if(i == 0 || w == 0){
+        table[i][w] = 0;
+      }else if(items[i-1].weight <= w){
+        //if the i^th (index i-1) item in the array fits this columns weight limit...
+        table[i][w] = Math.max(items[i-1].price + table[i-1][w-items[i-1].weight], table[i-1][w]);
+        /*
+        the value of a knapsack considering the first i items given and a max weight w
+        will be the maximum of:
+        the value of the newly added item PLUS the value in the same column that
+        only considers a weight low enough that the new item can fit exactly into the set without overflowing
+        */
       }else{
-        let newContents = [...this.contents];
-        let newPossible = [...this.possible];
-        newPossible.splice(index,1);
-        let newBag = new Bag(newContents, newPossible, this.weight, this.limit, this.price);
-        return newBag;
+        //if the item is too heavy on its own:
+        table[i][w] = table[i-1][w];
+        //this value in the table will be the same as before the item was considered
       }
     }
   }
-  //end class def
-  function bagSame(bag1, bag2){
-    let arr1 = bag1.contents.sort( (a,b) => {
-      return a.weight - b.weight === 0 ?
-        a.price - b.price :
-        a.weight - b.weight;
-    })
-    let arr2 = bag2.contents.sort( (a,b) => {
-      return a.weight - b.weight === 0 ?
-        a.price - b.price :
-        a.weight - b.weight;
-    })
-    if(arr1.length !== arr2.length) return false;
-    for(let i = 0; i < arr1.length; i++){
-      if(arr1[i].weight !== arr2[i].weight || arr1[i].price !== arr2[i].price){
-        return false
-      }
-    }
-    return true;
-  }
-  //end bagSame def
-
-  //start with empty bag
-  let bagList = [];
-  let startingBag = new Bag([],items,0,n,0);
-  bagList.push(startingBag);
-
-  function advPush(arr, bag){
-
-    let duplicate = false;
-    arr.forEach( obj => {
-
-      if (bagSame(obj,bag)){
-
-        duplicate = true;
-      }
-    })
-    if(!duplicate) arr.push(bag);
-    return !duplicate;
-  }
-
-  //add each possible addition to a list of bags
-  for(let k = 0; k < items.length; k++){
-    //console.log("...")
-    let newBagList = [];
-    bagList.forEach( bag => {
-      for(let j = 0; j < bag.possible.length; j++){
-        let fullerBag = bag.transfer(j)
-        if(fullerBag){
-          advPush(newBagList,fullerBag)
-        }
-      }
-    })
-    bagList = [...newBagList];
-  }
-  let res = bagList.sort( (a,b) => {
-    return b.price - a.price;
-  })[0].contents;
-  //console.log(res);
-  return res;
-  //if it is already on the list do not duplicate
+  console.log(table);
+  return table[length][n];
 }
